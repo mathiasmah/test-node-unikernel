0c04a5063e976829e47c1ce574b636b5f4ecddb9
Mathias Mahlknecht <mathiasmahlknecht@hotmail.com>
2019-08-20 16:45:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express    = require('express');
 var app = express();
 var directory = require('serve-index');
 
+console.log("hallo");
+
 app.use(express.static('./'));
 app.use(directory('./'));
 app.listen("8080");
